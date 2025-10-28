5a4481be3e051cbb13159f7267fbd69ccc8bc1f9
Linney34 <tarfedof@gmail.com>
2025-10-28 22:47:26 +0200

    Add files via upload

---
+++ FindProjects/FindProjects/src/main/java/com/ua/FindProjects/FindProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.ua.FindProjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindProjectsApplication.class, args);
+	}
+
+}
