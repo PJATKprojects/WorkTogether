c745129514dde32caadf5b92ef47d9fb0fa45c7d
Linney34 <tarfedof@gmail.com>
2025-10-29 21:16:51 +0200

    deleted unused import

--- FindProjects/FindProjects/src/main/java/com/ua/FindProjects/entitys/UserRating.java
+++ FindProjects/FindProjects/src/main/java/com/ua/FindProjects/entitys/UserRating.java
@@ -3,7 +3,6 @@ package com.ua.FindProjects.entitys;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
